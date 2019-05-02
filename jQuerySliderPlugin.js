2fbd6d057ff39377a00e48a03dc654f7bf742aba
SpekalsG3 <karpick.kirill@gmail.com>
2019-05-02 16:43:18 +0300

    Fixed default settings for each slider. Added starting point setting.

--- jQuerySliderPlugin.js
+++ jQuerySliderPlugin.js
@@ -1,10 +1,13 @@
 (function($) {
-	$.fn.myPlugin = function(min, max, step = 1, startPoint = 0, track = true, orientation = "horizontal", hud = true, interval = 4, hint = false) {
+	$.fn.myPlugin = function(min, max, step = 1, startPoint = min, track = true, orientation = "horizontal", hud = true, interval = 4, hint = false) {
 
 		this.startDragCheck = false;
 		step *= 260 / (max-min);
 
-		$(this)[0].children[1].children[0].innerHTML = startPoint;
+		this.each(function(i) {
+			this.children[1].children[0].innerHTML = startPoint;
+			this.children[1].style.left = (startPoint - min) * 260 / (max - min) + "px";
+		});
 
 		this.mousedown(function(e) {
 			if (!Object.values($(".pointer")).includes(e.target)) {
@@ -25,15 +28,9 @@
 				}*/
 
 				var move = Math.round((e.clientX - this.started - 8) / step) * step;
-				if (move > 0 && move < 260) {
+				if (move >= 0 && move <= 260) {
 					this.children[1].children[0].innerHTML = (max - min) * move / 260 + min; //move + min;
 					this.children[1].style.left = move + "px";
-				} else if (move <= step) {
-					this.children[1].children[0].innerHTML = min;
-					this.children[1].style.left = 0 + "px";
-				} else if (move + step >= 260) {
-					this.children[1].children[0].innerHTML = max;
-					this.children[1].style.left = 260 + "px";
 				}
 			}
 		});
@@ -48,4 +45,4 @@
 	}
 })(jQuery);
 
-$(".slider").myPlugin(100, 200, 10);
+$(".slider").myPlugin(100, 200, 10, 150);
