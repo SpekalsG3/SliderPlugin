1a66439a9162e876b44574f97420e3f2d1dba1f8
SpekalsG3 <karpick.kirill@gmail.com>
2019-05-03 16:43:51 +0300

    Added hud and it's visibility and interval settings.

--- jQuerySliderPlugin.js
+++ jQuerySliderPlugin.js
@@ -1,15 +1,24 @@
 (function($) {
-	$.fn.myPlugin = function(min, max, step = 1, startPoint = min, orientation = "horizontal", hint = false, hud = true, interval = 4, track = true) {
+	$.fn.myPlugin = function(min, max, step = 1, startPoint = min, orientation = "horizontal", hint = false, hud = true, interval = 5, track = true) {
 
 		this.startDragCheck = false;
 
 		this.each(function() {
-			this.children[1].children[0].innerHTML = startPoint;
+			this.onselectstart = function() {
+				return false;
+			}
+
+			this.children[1].children[0].innerHTML = min;
 			this.step = step * parseInt($(this.children[0]).css("width")) / (max-min);
 			if (startPoint > min && startPoint <= max) {
-				var indent = (startPoint - min) * parseInt($(this.children[0]).css("width")) / (max - min);
+				this.children[1].children[0].innerHTML = startPoint;
+				var indent = (startPoint - min) * parseInt($(this.children[0]).css("width")) / (max - min) + 8;
 				this.children[1].style.left = indent + "px";
 				this.children[0].children[0].style.width = indent + "px";
+			}
+
+			for (var i = 0; i < interval; i++) {
+				$(this.children[2]).append("<div>" + Math.floor(min + (max-min) / (interval-1) * i) + "</div>");
 			}
 
 			if (hint) {
@@ -22,6 +31,12 @@
 				this.children[0].children[0].style.display = "block";
 			} else {
 				this.children[0].children[0].style.display = "none";
+			}
+
+			if (hud) {
+				this.children[2].style.display = "flex";
+			} else {
+				this.children[2].style.display = "none";
 			}
 		});
 
@@ -37,12 +52,12 @@
 
 		this.mousemove(function(e) {
 			if (this.startDragCheck) {
-				var move = Math.round((e.clientX - this.started - 8) / this.step) * this.step;
+				var move = Math.round((e.clientX - this.started - 16) / this.step) * this.step;
 				
 				if (move >= 0 && move <= parseInt($(this.children[0]).css("width"))) {
 					this.children[1].children[0].innerHTML = (max - min) * move / parseInt($(this.children[0]).css("width")) + min;
-					this.children[1].style.left = move + "px";
-					this.children[0].children[0].style.width = move + "px";
+					this.children[1].style.left = move + 8 + "px";
+					this.children[0].children[0].style.width = move + 8 + "px";
 				}
 			}
 		});
@@ -57,4 +72,4 @@
 	}
 })(jQuery);
 
-$(".slider").myPlugin(100, 200, 10, 120, "horizontal", true, true, 4, true);
+$(".slider").myPlugin(0, 200, 5, 120, "horizontal", true, false, 4, true);
