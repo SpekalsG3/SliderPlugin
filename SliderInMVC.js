aecfe033fcd149c83d3bb30dc18e6f0ac62c0db6
SpekalsG3 <karpick.kirill@gmail.com>
2019-05-15 00:21:21 +0300

    Created main logic of mvc architecture

--- SliderInMVC.js
+++ SliderInMVC.js
@@ -3,7 +3,7 @@ var SliderController = function(sliderView, sliderModel) {
 	this.sliderModel = sliderModel;
 };
 
-SliderController.prototype.onStartDraging = function(e) {
+SliderController.prototype.startDraging = function(e) {
 	if (!Object.values($(".pointer")).includes(e.target)) {
 		this.sliderModel.startDraggingCheck = false;
 		return;
@@ -19,12 +19,25 @@ SliderController.prototype.onStartDraging = function(e) {
 }
 
 SliderController.prototype.init = function() {
-	this.sliderView.onStartDraging = this.onStartDragging.bind(this);
+
+	this.sliderView.startDraging = this.startDragging.bind(this);
+	this.sliderView.updateSlider = this.updateSlider.bind(this);
+	this.sliderView.endDragging = this.endDragging.bind(this);
 }
 
-SliderController.prototype.updateSlider = function(sliderModelData) {
+SliderController.prototype.updateSlider = function(e, sliderModelData) {
+	/*
+		started,
+		min,
+		max,
+		step,
+		color,
+		orientation,
+	*/
+
 	var sliderViewModel = {
-		color: sliderModelData.color
+		color: sliderModelData.color,
+		orientation: sliderModelData.orientation
 	};
 
 	var move;
@@ -34,28 +47,27 @@ SliderController.prototype.updateSlider = function(sliderModelData) {
 		move = Math.round((e.pageY - sliderModelData.started - 10) / sliderModelData.step) * sliderModelData.step;
 	}
 
-	sliderViewModel.trackSize = sliderModelData.move;
-
 	if (sliderModelData.orientation == "row") {
-		if (move >= 0 && move <= parseInt($(this.children[0]).css("width"))) {
-			sliderViewModel.value = Math.floor((sliderModelData.max - sliderModelData.min) * move / parseInt($(this.children[0]).css("width")) + sliderModelData.min);
-			sliderViewModel.pointerPosition = move + 8;
+		if (move >= 0 && move <= 260){//parseInt($(this.children[0]).css("width"))) {
+			sliderViewModel.value = Math.floor((sliderModelData.max - sliderModelData.min) * move / parseInt($(this.sliderView.element.children[0]).css("width")) + sliderModelData.min);
+
 			/*this.children[1].children[0].innerHTML = '<div style="border-top: 4px solid ' + this.mainColor + ';"></div>' + sliderViewModel.value;
 			this.children[1].style.left = this.pos + "px";
 			this.children[0].children[0].style.width = this.pos + "px";*/
 		}
 	} else {
-		if (move >= 0 && move <= parseInt($(this.children[0]).css("height"))) {
-			sliderViewModel.value = Math.floor((sliderModelData.max - sliderModelData.min) * move / parseInt($(this.children[0]).css("height")) + sliderModelData.min);
-			sliderViewModel.pointerPosition = move + 2;
+		if (move >= 0 && move <= 260){//parseInt($(this.children[0]).css("height"))) {
+			sliderViewModel.value = Math.floor((sliderModelData.max - sliderModelData.min) * move / parseInt($(this.sliderView.element.children[0]).css("height")) + sliderModelData.min);
+
 			/*this.children[1].children[0].innerHTML = '<div style="border-right: 4px solid ' + this.mainColor + ';"></div>' + sliderViewModel.value;
 			this.children[1].style.top = this.pos + "px";
 			this.children[0].children[0].style.height = this.pos + "px";*/
 		}
 	}
+	sliderViewModel.pointerPosition = move + 8;
 
-	this.sliderModel.value = sliderViewModel.value;
-	this.sliderModel.pointerPosition  = sliderViewModel.pointerPosition;
+	//this.sliderModel.value = sliderViewModel.value;
+	//this.sliderModel.pointerPosition  = sliderViewModel.pointerPosition;
 
 	this.sliderView.render(sliderViewModel);
 }
@@ -64,17 +76,58 @@ SliderController.prototype.endDragging = function() {
 	this.sliderModel.startDragCheck = false;
 }
 
-var SliderViewMock = function() {
-	this.calledRenderWith = null;
+var SliderView = function(element, params = {}) {
+	this.element = element;
+
+	this.parameters = {
+		min: params.min ? params.min : 0,
+		max: params.max ? params.max : 100,
+		step: params.step ? params.step : 1,
+		startPoint: params.startPoint ? params.startPoint : parameters.min,
+		orientation: params.orientation == "column" ? "column" : "row",
+		mainColor: params.mainColor ? params.mainColor : "#e85f3e",
+		hint: params.hint ? params.hint : true,
+		hud: params.hud ? params.hud : true,
+		interval: params.interval ? params.interval : 5,
+		track: params.track ? params.track : true,
+	}
+
+	this.startDraging = null;
+	this.updateSlider = null;
+	this.endDragging = null;
+}
+
+SliderView.prototype.render = function(sliderViewModel) {
+	if (sliderViewModel.orientation == "row") {
+		this.element.children[1].children[0].innerHTML = '<div style="border-top: 4px solid ' + sliderViewModel.color + ';"></div>' + sliderViewModel.value;
+		this.element.children[1].style.left = sliderViewModel.pointerPosition + "px";
+		this.element.children[0].children[0].style.width = sliderViewModel.pointerPosition + "px";
+	} else {
+		this.element.children[1].children[0].innerHTML = '<div style="border-right: 4px solid ' + sliderViewModel.color + ';"></div>' + sliderViewModel.value;
+		this.element.children[1].style.top = sliderViewModel.pointerPosition + "px";
+		this.element.children[0].children[0].style.height = sliderViewModel.pointerPosition + "px";
+	}
+}
+
+var parameters = {
+	min: 0,
+	max: 100,
+	step: 10,
+	startPoint: 50,
+	interval: 3
 };
 
-SliderViewMock.prototype.render = function(sliderViewModel) {
-	this.calledRenderWith = sliderViewModel;
+var view = new SliderView(document.getElementById("MVC"));
+
+var controller = new SliderController(view, null);
+
+var sliderModelData = {
+	started: 10,
+	min: 0,
+	max: 100,
+	step: 5,
+	color: "#e85f3e",
+	orientation: "row"
 };
 
-var view = new SliderViewMock(),
-	controller = new SliderController();
-
-var sliderViewModel = {
-
-};
+controller.updateSlider({pageX: 100, pageY: 100}, sliderModelData);
